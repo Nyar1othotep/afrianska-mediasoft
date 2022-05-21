fc2c6ada60943fe65a1c438c2473ee25b1072394
Minin Maxim <freelancerart@yandex.ru>
2022-05-21 19:24:16 +0300

    feat: add header and adaptive/scripts

--- src/index.js
+++ src/index.js
@@ -24,3 +24,48 @@ import rewriteData from './js/rewriteData'
 // 		  <object data="${icon}" type="image/svg+xml"></object>
 //      </div>`
 // }
+
+const burger = document.querySelector('.header__burger');
+const menu = document.querySelector('.header__menu');
+
+const closeMenuBurger = () => {
+    burger.classList.remove('active')
+    menu.classList.remove('active')
+    document.body.classList.remove('lock')
+}
+
+const closeOpenMenuBurger = () => {
+    burger.classList.toggle('active')
+    menu.classList.toggle('active')
+    document.body.classList.toggle('lock')
+}
+
+document.addEventListener('click', (event) => {
+    if (event.target.closest('.header__burger')) {
+        closeOpenMenuBurger()
+    }
+    if (event.target.closest('.menu-header__link')) {
+        closeMenuBurger()
+    }
+    if (!event.target.closest('.header__menu') && !event.target.closest('.header__burger')) {
+        closeMenuBurger()
+    }
+})
+
+const firstMenu = document.getElementById('first')
+const secondMenu = document.getElementById('second')
+
+document.addEventListener('click', (event) => {
+    if (event.target.closest('#first')) {
+        firstMenu.querySelector('.menu-header__nav').classList.toggle('active')
+        firstMenu.querySelectorAll('.menu-header__item').forEach(item => {
+            item.classList.toggle('active')
+        })
+    }
+    if (event.target.closest('#second')) {
+        secondMenu.querySelector('.menu-header__nav').classList.toggle('active')
+        secondMenu.querySelectorAll('.menu-header__item').forEach(item => {
+            item.classList.toggle('active')
+        })
+    }
+})
