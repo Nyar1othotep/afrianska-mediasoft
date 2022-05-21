744e13f72836a745e26d0ad3695562d28127effc
Minin Maxim <freelancerart@yandex.ru>
2022-05-21 11:23:56 +0300

    fix: connetcion images/icons and added all resources to images

--- src/index.js
+++ src/index.js
@@ -3,23 +3,24 @@ import './js/connectionImg'
 import './js/connectionIcons'
 import rewriteData from './js/rewriteData'
 
-let data = [{
-    img: 'logo.png',
-    icon: 'icon-home.svg'
-}, {
-    img: 'logo-footer.png',
-    icon: 'icon-lock.svg'
-}, {
-    img: 'logo.png',
-}]
+// let data = [{
+//     img: 'logo.png',
+//     icon: 'icon-home.svg'
+// }, {
+//     img: 'logo-footer.png',
+//     icon: 'icon-lock.svg'
+// }, {
+//     img: 'logo.png',
+//     icon: 'icon-lock.svg'
+// }]
 
-let newData = rewriteData(data)
+// let newData = rewriteData(data)
 
-for (let i = 0; i < newData.length; i++) {
-    let img = newData[i]['img']
-    let icon = newData[i]['icon']
-    gallery.innerHTML += `<div>
-    	 <img src="${img}" alt="">
-		  <object data="${icon}" type="image/svg+xml"></object>
-     </div>`
-}
+// for (let i = 0; i < newData.length; i++) {
+//     let img = newData[i]['img']
+//     let icon = newData[i]['icon']
+//     gallery.innerHTML += `<div>
+//     	 <img src="${img}" alt="">
+// 		  <object data="${icon}" type="image/svg+xml"></object>
+//      </div>`
+// }
